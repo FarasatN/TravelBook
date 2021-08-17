bfbce6dfdef21a41a1c955e704d643b07e0ec545
FarasatN <fesi3228f>
2021-08-17 17:49:40 +0400

    Room Database

--- app/src/main/java/com/farasatnovruzov/travelbook/MainActivity.java
+++ app/src/main/java/com/farasatnovruzov/travelbook/MainActivity.java
@@ -2,6 +2,7 @@ package com.farasatnovruzov.travelbook;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -15,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Room.databaseBuilder()
     }
 
     @Override
